d14a3eb5e7593aa37b845932be88b74c1f5cbd7b
angel <angelsol299@hotmail.com>
2019-06-28 14:35:33 +0200

    http requesting

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import Users from './components/users/Users';
 import axios from 'axios';
 
 class App extends Component {
-  componentDidMount() {
-    axios
-      .get('https://api.github.com/users')
-      .then(res => console.log(res.data));
+  state = {
+    users: [],
+    loading: false
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    console.log(res.data);
+    this.setState({ users: res.data, loading: false });
   }
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
-        <Users />
+        <Users loading={this.state.loading} users={this.state.users} />
       </div>
     );
   }
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,34 +2,10 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https:github.com/mojombo'
-      },
-      {
-        id: '2',
-        login: 'angel',
-        avatar_url:
-          'https://avatars3.githubusercontent.com/u/32056957?s=400&u=0f6fa6e9687f7bcb059e9f9d281a91d1baf42152&v=4',
-        html_url: 'https://github.com/angelsol299'
-      },
-      {
-        id: '3',
-        login: 'Dominic',
-        avatar_url:
-          'https://avatars3.githubusercontent.com/u/1519870?s=460&v=4',
-        html_url: 'https://github.com/trueadm'
-      }
-    ]
-  };
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
