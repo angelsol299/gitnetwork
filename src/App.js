7208bac3c942d120a547737163c15dcb01e08004
angel <angelsol299@hotmail.com>
2019-06-28 17:39:07 +0200

    functioning search

--- src/App.js
+++ src/App.js
@@ -10,21 +10,33 @@ class App extends Component {
     users: [],
     loading: false
   };
-  async componentDidMount() {
-    this.setState({ loading: true });
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+  //   // const res = await axios.get(
+  //   //   `https://api.github.com/users?client_id=${
+  //   //     process.env.REACT_APP_GITHUB_CLIENT_ID
+  //   //   }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   // );
+  //   // console.log(res.data);
+  //   // this.setState({ users: res.data, loading: false });
+  // }
+
+  //Search Github users
+  searchUsers = async text => {
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${
+      `https://api.github.com/search/users?q=${text}&client_id=${
         process.env.REACT_APP_GITHUB_CLIENT_ID
       }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
     console.log(res.data);
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
+
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
-        <Search />
+        <Search searchUsers={this.searchUsers} />
         <Users loading={this.state.loading} users={this.state.users} />
       </div>
     );
