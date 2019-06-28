b728f32792cc0eed3fb31a318f40c2f652f4c5c4
angel <angelsol299@hotmail.com>
2019-06-28 18:44:38 +0200

    adding an alert when the search is blank

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import axios from 'axios';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   //Search Github users
@@ -27,16 +29,25 @@ class App extends Component {
 
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  //setting alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 3500);
+  };
+
   render() {
+    const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
+        <Alert alert={this.state.alert} />
         <Search
           searchUsers={this.searchUsers}
           clearUsers={this.clearUsers}
-          showClear={this.state.users.length > 0 ? true : false}
+          showClear={users.length > 0 ? true : false}
+          setAlert={this.setAlert}
         />
-        <Users loading={this.state.loading} users={this.state.users} />
+        <Users loading={loading} users={this.state.users} />
       </div>
     );
   }
