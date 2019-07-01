3a1a9b68436594454521d4ceb0bc1e265ac0061f
angel <angelsol299@hotmail.com>
2019-07-01 20:49:23 +0200

    home and notFound components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
   return (
-    <nav className="navbar bg-primary">
+    <nav className="navbar bg-success" style={{ backgroundColor: '#3BD2C8' }}>
       <h1>
         <i className={icon} /> {title}
       </h1>
